f907c5e00a9f3d1cc7dfa339b0fe56af6aac368c
Jesper Quorning <jquorning@hotmail.com>
2019-02-18 12:15:29 +0100

    AWS separate library project.

--- Makefile
+++ Makefile
@@ -7,17 +7,34 @@
 ##    May you share freely, not taking more than you give.
 ##
 
-all: build test
+all: setup build test
 
-build:
+setup: setup-core setup-aws
+
+build: build-core build-aws
+
+clean: clean-core clean-aws
+
+build-core:
 	gprbuild -k dk8543.gpr
+
+build-aws:
+	gprbuild -k dk8543_aws.gpr
 
 test:
 	gprbuild -k dk8543_test.gpr
 
-clean:
+clean-core:
 	gprclean -q dk8543.gpr
+
+clean-aws:
+	gprclean -q dk8543_aws.gpr
+
+clean-test:
 	gprclean -q dk8543_test.gpr
 
-setup:
+setup-core:
 	tools/create-setup-adb.sh
+
+setup-aws:
+	tools/create-aws-setup-adb.sh
---
+++ tools/create-aws-setup-adb.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+base_directory=.
+temp_directory=$base_directory/var
+dest_directory=$base_directory/body_aws
+template=dk8543-aws-setup.adb.template
+destination=dk8543-aws-setup.adb
+date_time_utc=$(date -u "+%Y-%m-%dT%H:%M:%S")
+library_name=$(cat $temp_directory/LIBRARY_AWS_NAME)
+library_version=$(cat $temp_directory/LIBRARY_AWS_VERSION)
+
+uname_opt_m=$(uname -m)
+uname_opt_n=$(uname -n)
+uname_opt_p=$(uname -p)
+uname_opt_r=$(uname -r)
+uname_opt_s=$(uname -s)
+
+##
+##  Stream edit setup.adb.template into setup.adb
+##
+cat $temp_directory/$template                        | \
+sed 's/@_DATE_TIME_UTC_@/'$date_time_utc'/'     | \
+sed 's/@_LIBRARY_AWS_NAME_@/'$library_name'/'       | \
+sed 's/@_LIBRARY_AWS_VERSION_@/'$library_version'/' | \
+sed 's/@_UNAME_OPT_M_@/'$uname_opt_m'/'         | \
+sed 's/@_UNAME_OPT_N_@/'$uname_opt_n'/'         | \
+sed 's/@_UNAME_OPT_P_@/'$uname_opt_p'/'         | \
+sed 's/@_UNAME_OPT_R_@/'$uname_opt_r'/'         | \
+sed 's/@_UNAME_OPT_S_@/'$uname_opt_s'/'         | \
+cat >$dest_directory/$destination
+
+
