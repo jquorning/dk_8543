860d39594fc8d6bef387d79f1a6c15e9fd3c49ba
Jesper Quorning <jesper.quorning@gmail.com>
2019-03-09 05:31:21 +0100

    0.2.1 DK8543.Errors: File argument added. GNAT 2012

--- body_core/dk8543-errors.adb
+++ body_core/dk8543-errors.adb
@@ -7,23 +7,27 @@
 --    May you share freely, not taking more than you give.
 --
 
-with Ada.Text_IO;
+with Ada.Strings.Fixed;
 
 package body DK8543.Errors is
 
 
-   procedure Error (File_Name   : in String;
+   procedure Error (File        : in Ada.Text_IO.File_Type;
+                    File_Name   : in String;
                     Line_Number : in Natural;
                     Message     : in String)
    is
       use Ada.Text_IO;
+      use Ada.Strings;
+
+      Line_Number_Image : constant String := Fixed.Trim (Natural'Image (Line_Number), Left);
    begin
-      Put (Standard_Error, File_Name);
-      Put (Standard_Error, ":");
-      Put (Standard_Error, Natural'Image (Line_Number));
-      Put (Standard_Error, ": ");
-      Put (Standard_Error, Message);
-      New_Line (Standard_Error);
+      Put (File, File_Name);
+      Put (File, ":");
+      Put (File, Line_Number_Image);
+      Put (File, ": ");
+      Put (File, Message);
+      New_Line (File);
    end Error;
 
 
--- spec_core/dk8543-errors.ads
+++ spec_core/dk8543-errors.ads
@@ -7,10 +7,13 @@
 --    May you share freely, not taking more than you give.
 --
 
+with Ada.Text_IO;
+
 package DK8543.Errors is
 
 
-   procedure Error (File_Name   : in String;
+   procedure Error (File        : in Ada.Text_IO.File_Type;
+                    File_Name   : in String;
                     Line_Number : in Natural;
                     Message     : in String);
 
