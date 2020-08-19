f7b4258fc5e3c3885e628384edaee91ebd751fa7
Jesper Quorning <jesper.quorning@gmail.com>
2020-08-19 05:34:43 +0200

    Update .gitignore

---
+++ spec_ada/dk8543jq-strings.ads
@@ -0,0 +1,25 @@
+--
+--  The author disclaims copyright to this source code.  In place of
+--  a legal notice, here is a blessing:
+--
+--    May you do good and not evil.
+--    May you find forgiveness for yourself and forgive others.
+--    May you share freely, not taking more than you give.
+--
+
+with Ada.Strings;
+
+package DK8543JQ.Strings is
+
+   procedure Trim
+     (Item  : in     String;
+      First : in out Natural;
+      Last  : in out Natural;
+      Side  : in     Ada.Strings.Trim_End);
+
+   function In_First_Part
+     (From : in String;
+      Item : in String) return Boolean;
+   --  Return True when Item is first part of From.
+
+end DK8543JQ.Strings;
